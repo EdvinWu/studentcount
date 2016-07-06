94e372294414cb569ca786e22104796957fe324b
Edwin <e.malisevskis@gmail.com>
2016-07-06 12:05:09 +0300

    initial commit

---
+++ src/main/java/lv/ctco/StudentcountApplication.java
@@ -0,0 +1,12 @@
+package lv.ctco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentcountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentcountApplication.class, args);
+	}
+}
